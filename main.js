0c2709d5dee8d542b03195411551837d67849ead
alvin marco <alvinmarco8@gmail.com>
2023-11-13 23:02:11 +0700

    test nambah button done and delete

--- main.js
+++ main.js
@@ -29,32 +29,75 @@
         const bookList = document.getElementById('incompleteBookshelfList');
         bookList.innerHTML = '';
         for(let book of bookData) {
-            let row = document.createElement('article');
-            row.className = 'book_item'
-            row.innerHTML = '<h3>' + book.judul + '</h3><p>' + book.penulis + '<p>' + book.tahun + '<p>';
-            let actionDiv = document.createElement('div');
-            actionDiv.className = 'action';
-            actionDiv.innerHTML = '<button class="green">Selesai dibaca</button><button class="red">Hapus buku</button>';
-            row.appendChild(actionDiv);  
-            bookList.appendChild(row);
+            if (book.isComplete === false) {
+                let row = document.createElement('article');
+                row.className = 'book_item'
+                row.innerHTML = '<h3>' + book.judul + '</h3><p>' + book.penulis + '<p>' + book.tahun + '<p>';
+                let actionDiv = document.createElement('div');
+                actionDiv.className = 'action';
+                let selesaiButton = document.createElement('button');
+                selesaiButton.className = 'green';
+                selesaiButton.innerHTML = "Selesai Dibaca";
+                let hapusButton = document.createElement('button');
+                hapusButton.className = 'red';
+                hapusButton.innerHTML = "Hapus";
+                actionDiv.appendChild(selesaiButton);
+                actionDiv.appendChild(hapusButton);   
+                
+                selesaiButton.addEventListener('click', function () {
+                    addToCompleted(book.id)
+                });
+
+
+                row.appendChild(actionDiv);  
+                bookList.appendChild(row);
+            }
         }
     }
+
+    function addToCompleted (bookId) {
+        const bookTarget = findBook(bookId);
+        if (bookTarget == null) return;
+        bookTarget.isComplete = true;
+        document.dispatchEvent(new Event(RENDER_EVENT));
+    }
+
+    function findBook(bookId) {
+        for (const book of bookData) {
+          if (book.id === bookIdId) {
+            return book;
+          }
+        }
+        return null;
+      }
 
     function renderBookListComplete(){
         const bookData = getBookList();
         const bookList2 = document.getElementById('completeBookshelfList');
         bookList2.innerHTML = '';
         for(let book of bookData) {
-        let row = document.createElement('article');
-        row.className = 'book_item'
-        row.innerHTML = '<h3>' + book.judul + '</h3><p>' + book.penulis + '<p>' + book.tahun + '<p>';
-        let actionDiv = document.createElement('div');
-        actionDiv.className = 'action';
-        actionDiv.innerHTML = '<button class="green">Selesai dibaca</button><button class="red">Hapus buku</button>';
-        row.appendChild(actionDiv);  
-        bookList2.appendChild(row);
+            if (book.isComplete === true) {
+                let row = document.createElement('article');
+                row.className = 'book_item'
+                row.innerHTML = '<h3>' + book.judul + '</h3><p>' + book.penulis + '<p>' + book.tahun + '<p>';
+                let actionDiv = document.createElement('div');
+                actionDiv.className = 'action';
+                let selesaiButton = document.createElement('button');
+                selesaiButton.className = 'green';
+                selesaiButton.innerHTML = "Selesai Dibaca";
+                let hapusButton = document.createElement('button');
+                hapusButton.className = 'red';
+                hapusButton.innerHTML = "Hapus";
+                actionDiv.appendChild(selesaiButton);
+                actionDiv.appendChild(hapusButton);
+                // actionDiv.innerHTML = '<button class="green">Selesai dibaca</button><button class="red">Hapus buku</button>';
+                row.appendChild(actionDiv);  
+                bookList2.appendChild(row);
+            }
     }
     }
+
+
 
     submitAction.addEventListener('submit', function(event){
         event.preventDefault();
@@ -83,7 +126,8 @@
     window.addEventListener('load', function() {
         if(checkForStorage){
             if(localStorage.getItem(storageKey) !== null){
-                renderBookList();
+                renderBookListComplete();
+                renderBookListIncomplete();
             }
         } else{
                 alert('Browser tidak didukung!');
